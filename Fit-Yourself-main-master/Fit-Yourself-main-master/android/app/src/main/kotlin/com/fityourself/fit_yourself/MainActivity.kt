d4bc18eefd96c0e1748404665d7c4c7c124e24ba
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-04-17 00:39:29 +0600

    Delete Fit-Yourself-main-master/Fit-Yourself-main-master directory

--- Fit-Yourself-main-master/Fit-Yourself-main-master/android/app/src/main/kotlin/com/fityourself/fit_yourself/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.fityourself.fit_yourself
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Fit-Yourself-main-master/Fit-Yourself-main-master/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:fit_yourself/app.dart';
-
-void main() {
-  testWidgets('App starts without error', (WidgetTester tester) async {
-    await tester.pumpWidget(const FitYourselfApp());
-    expect(find.text('Fit Yourself by Exercise'), findsOneWidget);
-  });
-}
